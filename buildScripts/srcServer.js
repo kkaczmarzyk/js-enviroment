cea1d6a2ae30682da9e16785e5a3a55e651cad4f
Kacper Kaczmarzyk <kacper.kaczmarzyk@appincode.com>
2018-11-10 18:15:01 +0100

    8. Bundling

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { join } from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.get('/', function(req, res) {
+  res.sendFile(join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+  err
+    ? console.log(err)
+    : open('http://localhost:' + port);
+});
